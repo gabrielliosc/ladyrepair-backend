949ff21f383e17e399fa55747e2adf19462dd724
gabrielliosc <gabrielli.osc@gmail.com>
2024-03-30 23:42:34 -0300

    feat: remove index() method

--- src/controllers/ContractorController.ts
+++ src/controllers/ContractorController.ts
@@ -4,9 +4,6 @@ class ContractorController{
     create(req: Contractor, res: Response){
         //Cria usuário da fornecedora de serviço
     }
-    index(){
-        //Retornar informações de todas as fornecedoras de serviço cadastradas
-    }
     show(){
         //Retornar informações da fornecedora de serviço a partir do id
     }
