de456e1c36dd9dbec2a507c153c5e743fd2a38bf
gabrielliosc <gabrielli.osc@gmail.com>
2024-03-30 23:45:56 -0300

    feat: Update quotation controller

--- src/controllers/QuotationController.ts
+++ src/controllers/QuotationController.ts
@@ -2,13 +2,13 @@ import { Quotation } from "../interfaces/client.ts"
 
 class QuotationController{
     create(req: Quotation, res: Response){
-        //Cria a cotação do serviço da fornecedora de serviço para uma cliente a partir do id da cliente e da fornecedora
+        //Cria a orçamento do serviço da fornecedora de serviço para uma cliente a partir do id da cliente e da fornecedora
     }
     show(){
-        //Retornar informações da cotação a partir do id
+        //Retornar informações da orçamento a partir do id
     }
     delete(){
-        //Apaga as informações da cotação a partir do id
+        //Apaga as informações da orçamento a partir do id
     }
 }
 
