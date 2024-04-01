0165823378ed26f096be6eb7c462f313ae7164ba
gabrielliosc <gabrielli.osc@gmail.com>
2024-03-31 23:50:04 -0300

    feat: Add chat controller file

---
+++ src/controllers/ChatController.ts
@@ -0,0 +1,12 @@
+import { Chat } from "../interfaces/tables.ts"
+
+class ChatController {
+    create(req: Chat, res: Response){
+        //Criar uma mensagem entre cliente e prestadora
+    }
+    index(){
+        //Retornar todas as mensagens entre cliente e prestadora
+    }
+}
+
+export { ChatController }
