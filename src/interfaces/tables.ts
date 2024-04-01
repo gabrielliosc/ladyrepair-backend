f18e42eb042d85267e2d924cc323d5cca0f87c8a
gabrielliosc <gabrielli.osc@gmail.com>
2024-03-31 23:48:34 -0300

    feat: Add chat and recommendation interface

--- src/interfaces/tables.ts
+++ src/interfaces/tables.ts
@@ -50,3 +50,19 @@ export interface ServiceTypes {
     service_id: string, //Conferir
     name: string,
 }
+
+export interface Chat {
+    id?: number
+    client_id: number,
+    contractor_id: number,
+    message: string,
+    direction: string,
+    send_date: Date
+}
+
+export interface Recommendation {
+    id?: number
+    contractor_from_id: number,
+    contractor_to_id: number,
+    message: string
+}
